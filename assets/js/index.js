dc275d248bb37974f28cbc4114db1ff2cfff8530
KhoaHenry <trananhkhoa73@gmail.com>
2021-11-30 12:46:06 +0700

    fix directory

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,3 @@
 document.getElementById("heros-btn").onclick = function () {
-    console.log("clicked")
-    window.location.href = "/editors/cv-1.html"
+    window.location.href = "editors/cv-1.html"
 }
